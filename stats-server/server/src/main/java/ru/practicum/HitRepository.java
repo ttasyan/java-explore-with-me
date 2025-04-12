e86027e6f43e5684d059b6b5b4e22ec939f4ae1a
Avdulova <ttasina00@mail.ru>
2025-04-12 14:15:29 +0300

    подключение к бд

--- stats-server/server/src/main/java/ru/practicum/HitRepository.java
+++ stats-server/server/src/main/java/ru/practicum/HitRepository.java
@@ -10,13 +10,13 @@ import java.util.List;
 
 public interface HitRepository extends JpaRepository<EndpointHit, Integer> {
     @Query("SELECT new ru.practicum.stats.dto.StatsResponse(h.app, h.uri, COUNT(h)) FROM EndpointHit h WHERE h.timestamp BETWEEN :start AND :end " +
-            "AND (:uris IS NULL OR h.uri IN :uris) GROUP BY h.uri ORDER BY COUNT(h) DESC")
+            "AND (:uris IS NULL OR h.uri IN :uris) GROUP BY h.app, h.uri ORDER BY COUNT(h) DESC")
     List<StatsResponse> countHits(@Param("start") LocalDateTime start,
                                   @Param("end") LocalDateTime end,
                                   @Param("uris") List<String> uris);
 
     @Query("SELECT new ru.practicum.stats.dto.StatsResponse(h.app, h.uri, COUNT(DISTINCT h.ip)) FROM EndpointHit h WHERE h.timestamp BETWEEN :start AND :end " +
-            "AND (:uris IS NULL OR h.uri IN :uris) GROUP BY h.uri ORDER BY COUNT(DISTINCT h.ip) DESC")
+            "AND (:uris IS NULL OR h.uri IN :uris) GROUP BY h.app, h.uri ORDER BY COUNT(DISTINCT h.ip) DESC")
     List<StatsResponse> countUniqueHits(@Param("start") LocalDateTime start,
                                         @Param("end") LocalDateTime end,
                                         @Param("uris") List<String> uris);
